a5f1fc0a02a3115100a1456a76849019fa4508f6
xmmrh <496249311@qq.com>
2018-01-25 23:35:39 +0800

    new

--- js/commen.js
+++ js/commen.js
@@ -20,37 +20,36 @@ $(function () {
             if(clientW < 768){
                 className = "backColor-mobile";
             }
-            window.onscroll = function () {
 
-                if (scroll().top >= barTop || scroll().top<=0) {
+            $(window).scroll(function () {
+                if (scroll().top >= barTop || scroll().top <= 0) {
                     nav.bar.className = "nav-bar fixed";
                     nav.adbar.style.marginTop = adBarTop + nav.bar.offsetHeight + "px";
                 } else {
                     nav.bar.className = "nav-bar";
                     nav.adbar.style.marginTop = 0;
                 }
-                if(nav.picId){
+                if (nav.picId) {
                     nav.adbar.style.marginTop = 0;
                     var picHeight = nav.picId.offsetHeight;
-                    if(scroll().top>barTop+picHeight){
+                    if (scroll().top > barTop + picHeight) {
                         $(nav.bar).addClass(className);
-                    }else{
+                    } else {
                         $(nav.bar).removeClass(className);
                     }
-                }else{
+                } else {
                     $(nav.bar).addClass(className);
                 }
                 //左侧菜单栏的fixed定位
-                if(menu.$menu.length >0){
+                if (menu.$menu.length > 0) {
                     menu.fnMenu();
                 }
-            }
+            });
 
         }
     };
 
     nav.fixed();
-
 
 });
 
@@ -60,15 +59,15 @@ menu ={
     fnColor : function () {
         $.each(menu.$link,function (i,n) {
             var $n = $(n);
-            var rgbColor  = $(n).find(".color-block").css("background-color");
-            if($(n).hasClass("active")){
+            var rgbColor  = $n.find(".color-block").css("background-color");
+            if($n.hasClass("active")){
                 $n.css("background-color",rgbColor);
             };
             n.onmouseover = function () {
                 $n.css("background-color",rgbColor);
             };
             n.onmouseout = function () {
-                if(!$(n).hasClass("active")){
+                if(!$n.hasClass("active")){
                     $n.css("background-color","transparent");
                 }
             };
@@ -95,6 +94,32 @@ menu ={
     },
     init : function () {
         menu.fnColor();
+        menuContent.init();
+    }
+};
 
+menuContent = {
+    $carousel : $('#carousel-ol'),
+    $hiddenBox : $('.tech-content .hidden-box'),
+    $menuLi : $('.menu-bar li.active'),
+    initItemColorFn : function () {
+        if(menuContent.$hiddenBox.length > 0) {
+            var rbgColor = menuContent.$menuLi.css("background-color");
+            $.each(menuContent.$hiddenBox, function (i, n) {
+                n.style.backgroundColor = rbgColor;
+            });
+        }
+    },
+    initCarouselColorFn : function () {
+        if(menuContent.$carousel.length > 0){
+            var rbgColor = menuContent.$menuLi.css("background-color");
+            console.log(rbgColor);
+            var $style = $('<style>.carousel-indicators .active{border-color:'+rbgColor+'}</style>');
+            $('head').append($style);
+        }
+    },
+    init : function () {
+        menuContent.initItemColorFn();
+        menuContent.initCarouselColorFn();
     }
 };
